4af5e716099001e44f1f237d81f90d6ab8789a1d
NAbinav <abinav0506@gmail.com>
2024-09-12 00:15:30 +0530

    responsive 6.0

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { MdMenu } from "react-icons/md";
 import { RiCloseFill } from "react-icons/ri";
 const Navbar = () => {
   const [menu, setmenu] = useState(false);
+  const cl = menu ? "display box flex-col" : "hidden flex-col";
   return (
     <>
       <div className="Nav bodoni-moda hidden sm:block">
@@ -27,12 +28,12 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="Nav bodoni-moda block sm:hidden">
+      <div className="Nav block sm:hidden">
         <ul>
           <li className="ele Logo ">
             <Image src={Logo} height={75} width={75}></Image>
           </li>
-          <li className="ele">
+          <li className="ele text-cyan-50">
             {menu ? (
               <RiCloseFill
                 onClick={() => {
@@ -48,6 +49,20 @@ const Navbar = () => {
                 }}
               />
             )}
+          </li>
+        </ul>
+        <ul className={cl}>
+          <li className="ele">
+            <p>Home</p>
+          </li>
+          <li className="ele">
+            <p>Event</p>
+          </li>
+          <li className="ele">
+            <p>Sponsers</p>
+          </li>
+          <li className="ele">
+            <a href="https://vitchennaievents.com/">Register</a>
           </li>
         </ul>
       </div>
