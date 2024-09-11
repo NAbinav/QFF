1478f7786801c56608b2af79988be0221855bdd7
NAbinav <abinav0506@gmail.com>
2024-09-12 00:27:55 +0530

    .

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,13 +1,22 @@
 "use client";
+import { type } from "os";
 import Typed from "../components/Typing";
+import { motion, spring } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="Home">
-      <h1 className="text-6xl max-sm:text-4xl">
-        Welcome to <span className="heading ">Qiskit Fall Fest 2024</span>
-      </h1>
-      <Typed />
-    </div>
+    <motion.div
+      className="fixed"
+      initial={{ x: "-100%", filter: "blur(10px)" }}
+      animate={{ x: "0", filter: "blur(0px)" }}
+      transition={spring}
+    >
+      <div className="Home">
+        <h1 className="text-6xl max-sm:text-4xl">
+          Welcome to <span className="heading ">Qiskit Fall Fest 2024</span>
+        </h1>
+        <Typed />
+      </div>
+    </motion.div>
   );
 }
