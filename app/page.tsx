dc0175fd2e8988e731811651e9ba63f974f59345
NAbinav <abinav0506@gmail.com>
2024-09-13 11:26:59 +0530

    profile

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,32 @@ import p1 from "@/public/profile1.png";
 import Profile from "@/components/profile";
 const page = () => {
   return (
-    <div className="sm:text-xs" id="home">
+    <div className="sm:text-xs flex flex-col gap-40" id="home">
       <Home></Home>
       <div className="flex justify-center my-10" id="events">
         <Card></Card>
       </div>
-      <div className="flex gap-6 justify-evenly max-sm:justify-center max-sm:flex-col max-sm:w-full max-sm:text-center ">
+      <div className="h-full flex w-full flex-grow flex-nowrap self-center text-center  gap-6 justify-evenly max-sm:justify-center max-sm:flex-col max-sm:w-[400px] max-sm:text-center ">
         <Profile
           props={{
             name: "Kailash Shankar",
             dept: "Designing Lead",
+            branch: "ECM",
+            profile: p1,
+          }}
+        />
+        <Profile
+          props={{
+            name: "Abinav N",
+            dept: "Web Developer",
+            branch: "ECM",
+            profile: p1,
+          }}
+        />
+        <Profile
+          props={{
+            name: "Abinav N",
+            dept: "Web Developer",
             branch: "ECM",
             profile: p1,
           }}
--- components/profile.jsx
+++ components/profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 const profile = ({ props }) => {
   return (
-    <div className="flex max-sm:self-center flex-col snap-center justify-evenly align-middle text-center items-center font-[300] text-base gap-5  h-[300px] w-[500px] border-[1px] border-white bg-[rgba(255,255,255,0.1)] rounded-xl group hhover:bg-gradient-to-r hhover:from-[rgb(61,106,255,0.14)] hhover:to-[rgb(255,56,212,0.163)] max-sm:h-28 max-sm:w-full    max-sm:flex-row profilecard">
+    <div className=" profilecard flex max-sm:self-center flex-col snap-center max-sm:justify-evenly align-middle text-center items-center font-[300] text-base gap-5  h-[300px] w-[500px] border-[1px] border-white bg-[rgba(255,255,255,0.1)] rounded-xl group hhover:bg-gradient-to-r hhover:from-[rgb(61,106,255,0.14)] hhover:to-[rgb(255,56,212,0.163)] max-sm:h-28 max-sm:w-full    max-sm:flex-row profilecard">
       <Image
         className="rounded-full w-32 h-32   border-white border-2 group-hover:w-36 group-hover:h-36 transition-all duration-300 max-sm:w-16 max-sm:h-16 max-sm:group-hover:w-16 max-sm:group-hover:h-16"
         src={props.profile}
