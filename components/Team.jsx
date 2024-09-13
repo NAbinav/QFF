8f1df07aec51572cbe4b70fefda1c8ae534da72d
NAbinav <abinav0506@gmail.com>
2024-09-13 15:39:20 +0530

    about

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Profile from "./profile";
 import p1 from "@/public/profile1.png";
+import p2 from "@/public/profile2.jpg";
+
 const Team = () => {
   return (
     <div className="h-full flex   flex-grow flex-nowrap self-center text-center  gap-6 justify-center max-sm:justify-center max-sm:flex-col max-sm:text-center max-sm:snap-center align-middle max-sm:self-center max-sm:items-center">
@@ -15,10 +17,9 @@ const Team = () => {
         />
         <Profile
           props={{
-            name: "Abinav N",
-            dept: "Web Developer",
-            branch: "ECM",
-            profile: p1,
+            name: "Aurum Joshi",
+            dept: "Project Lead",
+            profile: p2,
           }}
         />
       </div>
