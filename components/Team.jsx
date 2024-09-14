c8c08a8c66fd10823c20be02604a43137b1462b5
NAbinav <abinav0506@gmail.com>
2024-09-14 16:39:24 +0530

    kailash

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Profile from "./profile";
-import p1 from "@/public/profile1.png";
 import p2 from "@/public/profile2.jpg";
 import p3 from "@/public/profile3.png";
-
+import p4 from "@/public/profile4.jpg";
+import p5 from "@/public/profile5.jpg";
 const Team = () => {
   return (
     <div className="h-full flex   flex-grow flex-nowrap self-center text-center  gap-6 justify-center max-sm:justify-center max-sm:flex-col max-sm:text-center max-sm:snap-center align-middle max-sm:self-center max-sm:items-center">
@@ -12,7 +12,7 @@ const Team = () => {
           props={{
             name: "Kailash Shankar",
             dept: "Designing Lead",
-            profile: p1,
+            profile: p4,
           }}
         />
         <Profile
@@ -35,7 +35,7 @@ const Team = () => {
           props={{
             name: "xxxx",
             dept: "yyyyyy",
-            profile: p1,
+            profile: p5,
           }}
         />
       </div>
