f48a52a29c86c3106643c25ba639e0458b5e018c
NAbinav <abinav0506@gmail.com>
2024-09-15 20:25:27 +0530

    poster

---
+++ app/poster/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import poster from "../../public/Qiskit Fall Fest Poster.png";
+import { RiCloseFill } from "react-icons/ri";
+const page = () => {
+  return (
+    <div className="flex flex-row justify-center snap-center self-center items-center ">
+      <Image src={poster} width={400}></Image>
+      <RiCloseFill />
+    </div>
+  );
+};
+
+export default page;
--- components/Poster.jsx
+++ components/Poster.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import Modal from "react-modal";
 import Image from "next/image";
 import poster from "../public/image.png";
 const Poster = () => {
+  const [showModal, setShowModal] = useState(false);
+  showModal
+    ? (parent.document.body.style.overflow = "hidden")
+    : (parent.document.body.style.overflow = "auto");
   return (
     <div className="flex flex-col justify-center gap-5">
       <div className="heading text-6xl text-center">POSTER</div>
-      <Image src={poster} alt="poster" height={500}></Image>
+      <a href="/poster">
+        <Image src={poster} alt="poster" height={500}></Image>
+      </a>
     </div>
   );
 };
