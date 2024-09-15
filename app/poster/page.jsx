e834e789ff0d7abb2acf454a0f7802cb6c14a162
NAbinav <abinav0506@gmail.com>
2024-09-15 21:37:52 +0530

    poster

--- app/poster/page.jsx
+++ app/poster/page.jsx
@@ -4,9 +4,13 @@ import poster from "../../public/Qiskit Fall Fest Poster.png";
 import { RiCloseFill } from "react-icons/ri";
 const page = () => {
   return (
-    <div className="flex flex-row justify-center snap-center self-center items-center ">
-      <Image src={poster} width={400} alt="poster"></Image>
-      <RiCloseFill />
+    <div className="flex flex-row text-3xl  justify-center ">
+      <a href="/" className="absolute top-16">
+        <RiCloseFill />
+      </a>
+      <div className="flex flex-row justify-center snap-center self-center items-center translate-y-24 ">
+        <Image src={poster} width={400} alt="poster"></Image>
+      </div>
     </div>
   );
 };
--- components/Poster.jsx
+++ components/Poster.jsx
@@ -4,9 +4,14 @@ import poster from "../public/image.png";
 const Poster = () => {
   return (
     <div className="flex flex-col justify-center gap-5">
-      <div className="heading text-6xl text-center">POSTER</div>
+      <div className="heading text-6xl text-center ">POSTER</div>
       <a href="/poster">
-        <Image src={poster} alt="poster" height={500}></Image>
+        <Image
+          src={poster}
+          alt="poster"
+          height={500}
+          className="flex justify-center items-center snap-center self-center align-middle"
+        ></Image>
       </a>
     </div>
   );
