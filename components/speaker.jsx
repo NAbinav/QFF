e7f1f5f839e08bacfe39c6cbfa29f1701786aa78
NAbinav <abinav0506@gmail.com>
2025-09-16 22:24:21 +0530

    2025 update padding

--- components/speaker.jsx
+++ components/speaker.jsx
@@ -5,8 +5,8 @@ import Profile from "@/components/speaker_profile";
 import p1 from "@/public/speaker1.jpeg";
 const Team = () => {
   return (
-    <div className="speakers-page min-w-full">
-      <div className="heading text-4xl max-sm:text-xl">SPEAKERS</div>
+    <div className=" min-w-full">
+      <div className="heading text-6xl max-sm:text-xl pl-20">SPEAKERS</div>
       <div className="flex max-sm:flex-col justify-center">
         <div className=" flex flex-grow flex-wrap self-center text-center  gap-6 justify-around max-w-4xl max-sm:justify-center max-sm:flex-col max-sm:text-center max-sm:snap-center align-middle max-sm:self-center max-sm:items-center my-14">
           
